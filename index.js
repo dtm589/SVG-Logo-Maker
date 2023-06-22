bbdd18fa99f05de5ea2d344f8e8fd16bba30d540
Derek Marchese <derekmarchese1@gmail.com>
2023-06-22 11:56:36 -0400

    worked on the inquirer section

--- index.js
+++ index.js
@@ -2,3 +2,42 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Square, Circle } = require('./lib/shapes');
+
+//inquirer prompt 
+function promptUser() {
+    inquirer.prompt([
+        {
+            type:'input',
+            message:'What three letters do you want in your logo?',
+            name: 'text'
+        },
+        {
+            type: 'input',
+            message: 'What should the color of the text be?',
+            name: 'textColor',
+        },
+        {
+            type: 'list',
+            message: 'What shape should the logo be?',
+            choices: ['Triangle', 'Square', 'Circle'],
+            name: 'shape',
+        },
+        {
+            type: 'input',
+            message: 'What should the color of the background be?',
+            name: 'shapeBackgroundColor',
+        },
+    ])
+    .then((answers) => {
+        //Check only three letters were entered for text variable
+        if (answers.text.length > 3) {
+            console.log("Only three letters can be displayed in the logo.");
+            promptUser();
+        } else {
+            writeToFile('logo.svg', answers);
+        }
+    });
+}
+
+//Call function to start app
+promptUser();
